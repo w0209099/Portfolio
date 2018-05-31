b44df7e3795436d53f84fa67572fed23f930e567
w0209099 <w0209099@nscc.ca>
2018-05-31 09:55:17 -0300

    third commit

--- Portfolio/resources/views/Home.blade.php
+++ Portfolio/resources/views/Home.blade.php
@@ -5,21 +5,24 @@
 
     <section id = "contant">
         <div id = "left">
-            <div class ="welcome"><p>Hello My name is Andrew Schnurr. Welcome to my online portfolio </p></div>
-            <div class ="welcome"><p>I am A Nova Scotia Community College trained Web Application Developer, with some understanding of Web Design.</p></div>
-            <div class ="welcome">
-                <p>To learn about me please see my <a href="{{ url('/About') }}">About</a> page</p>
-            </div>
-            <div class ="welcome">
-                <p>View my <a href="{{ url('/Resume') }}">Resume</a> Page to see what skills i can offer.</p>
-            </div>
-            <div class ="welcome">
-                <p>See <a href="{{ url('/Work') }}">My work</a> page for some examples of my work and a more in depth look at how i make my portfolio</p>
-            </div>
-            <div class ="welcome">
-                <p>To contact me to arrange a meeting, ask me questions or simply leave a comment about my site, plaease see
-                    my <a href="{{ url('/Contact') }}">Contact Information</a> here/p>
-            </div>
+            <p class="section">Hello My name is Andrew Schnurr. I am A Nova Scotia Community College trained Web Application Developer,
+                with some understanding of Web Design</p>
+            <p class="section">Welcome to the current version of my portfolio</p>
+            <p class="section">I say the current version because I do not believe I will ever stop modifying,
+                changing or adding to my portfolio. There are many things and features  I would like to add yet am unable to at this moment
+                because I lack the skills or the time accomplish them or they might simply not be needed at this moment.  </p>
+            <p class="section">In the mean time please feel free to look around my site. If you wish to arrange a meeting,
+                ask me questions or simply leave a comment you can find my Contact information
+                <a href="{{ url('/Contact') }}">here</a></p>
+        </div>
+
+        <div id="right">
+
+            <p class="section">To learn about me please see my <a href="{{ url('/About') }}">About</a> page</p>
+
+            <p class="section">View my <a href="{{ url('/Resume') }}">Resume</a> Page to see what skills i can offer.</p>
+
+            <p class="section">See <a href="{{ url('/Work') }}">My work</a> page for some examples of my work and a more in depth look at how i make my portfolio</p>
         </div>
     </section>
 
--- Portfolio/resources/views/Work.blade.php
+++ Portfolio/resources/views/Work.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 
-    <div class="accordionItem" class="clearfix">
+    <div class="accordionItem" >
         <h3 id="head1" onclick=open1();>Early Work</h3>
         <section id = "ws1" class="clearfix">
-            <div id = "ws1left" class="clearfix">
-                <p>Click <a href="{{ url('/CupCake') }}">here</a> to see the CupCake web view</p>
-                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/cupecake/cupecake.html">here</a> to see the HTML code</p>
-                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/cupecake/cupecake.css">here</a> to see the CSS code</p>
+            <div id = "ws1left" >
+                <p>conference center</p>
                 <p>Click <a href="{{ url('/Center') }}">here</a> to see the conference center web view</p>
-                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/cupecake/cupecake.html">here</a> to see the HTML code</p>
-                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/cupecake/cupecake.css">here</a> to see the CSS code</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/conference%20center/center.htm">here</a> to see the conference center HTML code</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/conference%20center/center.css">here</a> to see the conference center 1st CSS code</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/conference%20center/table.css">here</a> to see the conference center 2nd CSS code</p>
+                <p>CupCake</p>
+                <p>Click <a href="{{ url('/CupCake') }}">here</a> to see the CupCake web view</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/cupecake/cupecake.html">here</a> to see the CupCake HTML code</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/blob/master/cupecake/cupecake.css">here</a> to see the CupCake CSS code</p>
             </div>
-            <div id = "ws1right">
-                <p>
-
-                </p>
+            <div id = "ws1right" >
+                <p>Some of my earliest work the conference center and cupcake pages are simple designs ability to use and understand the fundamental basics of web development.</p>
+                <p>The conference center schedule is a basic web page the shows the schedule of a conference center in a simple row/column setup using only HTML5 and limited CSS3.</p>
+                <p>The Cupcake site is a simple bakery page with pictures and placeholder text. It also has basic Responsive design thought media queries.</p>
             </div>
         </section>
     </div>
@@ -24,14 +27,17 @@
     <br>
     <div class="accordionItem" class="clearfix">
         <h3 id="head2" onclick=open2(); >Laravel Database</h3>
-        <section id = "ws2" >
-            <div id = "ws2left" class="clearfix">
-                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/Portfolio">here</a> to see My Protfolio's code</p>
+        <section id = "ws2" class="clearfix">
+            <div id = "ws2left">
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/project">here</a> to see My Database projects github</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/project/database/migrations">here</a> to see My Database projects migrations</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/project/app">here</a> to see My Database projects models</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/project/resources/views">here</a> to see My Database projects views</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/project/app/Http/Controllers">here</a> to see My Database projects Controllers</p>
             </div>
             <div id = "ws2right">
-                <p>
-
-                </p>
+                <p>My Laravel Database project is a functional MySQL database built with the Laravel framework as administrative back-end of a potential Content Management System. </p>
+                <p>The system has Full Create Read Update Delete functionality on multiply database tables and datatypes as well as has a fully functional Signup and login system that restricts functionality based of the user’s permissions.</p>
             </div>
         </section>
     </div>
@@ -41,12 +47,16 @@
         <h3 id="head3" onclick=open3(); >My Portfolio</h3>
         <section id ="ws3" class = "clearfix">
             <div id = "ws3left" class="clearfix">
-                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/project">here</a> to see My Database projects code code</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/Portfolio">here</a> to see My Protfolio's github</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/Portfolio/resources/views">here</a> to see My Protfolio's views</p>
+                <p>Click <a href="https://github.com/w0209099/Portfolio/tree/master/Portfolio/public/css">here</a> to see My Protfolio's CSS</p>
             </div>
             <div id = "ws3right">
-                <p>
+                <p>My portfolio is intended to show off all my skills in the field of web development</p>
+                <p>It currently only use’s HTML5, CSS3 and some bootstrap,
+                    because I felt that at this point adding on other features such as jQueryUI elements would not be effective do to my lack of current artistic design skill,
+                    though I hope to add these  features and others at a later date.</p>
 
-                </p>
             </div>
         </section>
     </div>
